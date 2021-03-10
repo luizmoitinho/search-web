a1c2fa110232a2d8a34cf0de39e944a784d730d2
luizmoitinho <luizcarlos.moitinho@medlynx.com.br>
2021-03-10 13:55:46 -0300

    buscando ips na web

--- app/app.go
+++ app/app.go
@@ -1,6 +1,12 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 //Generate ... return app line comand to execute
 func Generate() *cli.App {
@@ -8,5 +14,32 @@ func Generate() *cli.App {
 	app.Name = "Aplicação de linha de comando"
 	app.Usage = "Busca IPs e Nome de servidores na Web"
 
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Buscar IPs de endereço na Web",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "google.com.br",
+				},
+			},
+			Action: searchIps,
+		},
+	}
+
 	return app
+}
+
+func searchIps(c *cli.Context) {
+	host := c.String("host")
+
+	//net
+	ips, err := net.LookupIP(host)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
