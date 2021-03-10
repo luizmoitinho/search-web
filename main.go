0124f515144c8e154f21169412e78109d7628948
luizmoitinho <luizcarlos.moitinho@medlynx.com.br>
2021-03-10 13:49:12 -0300

    permitir o envio de parametro por linha de comando

--- main.go
+++ main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"search-web/app"
 )
 
 func main() {
-	fmt.Println("Search Web")
+	fmt.Println("# Search Web")
+
 	aplicacao := app.Generate()
+	if err := aplicacao.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
 
 }
