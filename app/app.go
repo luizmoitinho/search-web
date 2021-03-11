34d66be37664febf537a72dfc7d4d6d22aae700b
luizmoitinho <luizcarlos.moitinho@medlynx.com.br>
2021-03-11 12:48:37 -0300

    rotina para buscar os servidores de um host

--- app/app.go
+++ app/app.go
@@ -14,31 +14,53 @@ func Generate() *cli.App {
 	app.Name = "Aplicação de linha de comando"
 	app.Usage = "Busca IPs e Nome de servidores na Web"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "google.com.br",
+		},
+	}
+
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Buscar IPs de endereço na Web",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "google.com.br",
-				},
-			},
+			Name:   "ip",
+			Usage:  "Buscar IPs de endereço na Web",
+			Flags:  flags,
 			Action: searchIps,
+		},
+		{
+			Name:   "servers",
+			Usage:  "Busca nome dos servidores na Web",
+			Flags:  flags,
+			Action: searchServers,
 		},
 	}
 
 	return app
 }
 
-func searchIps(c *cli.Context) {
+func searchServers(c *cli.Context) {
 	host := c.String("host")
+	servers, err := net.LookupNS(host)
 
-	//net
-	ips, err := net.LookupIP(host)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	for _, server := range servers {
+		fmt.Println(server.Host)
+	}
+
+}
+
+func searchIps(c *cli.Context) {
+	host := c.String("host")
+	ips, err := net.LookupIP(host)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	for _, ip := range ips {
 		fmt.Println(ip)
 	}
